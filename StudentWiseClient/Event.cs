2bdc28dc90a8243afe29a9c4b5ffd88d7e006a58
karinakozarova <karinakozarova1303@gmail.com>
2020-01-01 17:19:59 +0100

    added GUI and validations for creating new event

---
+++ StudentWiseClient/Event.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentWiseClient
+{
+    enum EventType
+    {
+        OTHER,
+        DUTY,
+        PARTY
+    }
+
+    class Event
+    {
+        public Event(String title, String description, DateTime start, DateTime end)
+        {
+            this.EventType = EventType.OTHER;
+            this.Title = title;
+            this.Description = description;
+            this.EndsAt = end;
+            this.StartsAt = start;
+        }
+
+        public EventType EventType
+        {
+            get
+            {
+                return this.EventType;
+            }
+            set
+            {
+                if(Enum.IsDefined(typeof(EventType), value))
+                {
+                    this.EventType = value;
+                }
+            }
+        }
+
+        public String Title
+        {
+            get
+            {
+                return this.Title;
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    return;
+                }
+                this.Title = value;
+            }
+        }
+
+        public String Description
+        {
+            get
+            {
+                return this.Description;
+            }
+            set{
+                if(String.IsNullOrEmpty(value)){
+                    return;
+                }
+                this.Description = value;
+            }
+        }
+
+        public DateTime StartsAt
+        {
+            get;
+            set;
+        }
+
+        public DateTime EndsAt
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ StudentWiseClient/NewEvent.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentWiseClient
+{
+    public partial class NewEvent : Form
+    {
+        /// <summary>
+        /// Disables Datetime pickers past dates
+        /// </summary>
+        private void DisablePastDates()
+        {
+            // make sure you cant add past events
+            endDttpkr.MinDate = DateTime.Today;
+            startDttpkr.MinDate = DateTime.Today;
+        }
+
+        public NewEvent()
+        {
+            InitializeComponent();
+            DisablePastDates();
+        }
+
+        private void CreateBttn_Click(object sender, EventArgs e)
+        {
+            // check that the fields are populated
+            if (String.IsNullOrEmpty(titleTbx.Text))
+            {
+                MessageBox.Show("Enter a title");
+                return;
+            }
+
+            if (String.IsNullOrEmpty(descriptionTbx.Text))
+            {
+                MessageBox.Show("Enter a description");
+                return;
+            }
+
+
+            // create new event object
+            Event newEvent = new Event(titleTbx.Text, descriptionTbx.Text, startDttpkr.Value, endDttpkr.Value);
+
+            // TODO: send event object to the API
+        }
+    }
+}
